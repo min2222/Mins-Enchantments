e88af4a3b2ec5233d28e9a534f12abf78971be21
min <goyoumin2@gmail.com>
2024-01-22 23:49:00 +0900

    fixed end and sculk enchantment

--- src/main/java/com/min01/minsenchantments/enchantment/end/AbstractEndEnchantment.java
+++ src/main/java/com/min01/minsenchantments/enchantment/end/AbstractEndEnchantment.java
@@ -1,10 +1,11 @@
 package com.min01.minsenchantments.enchantment.end;
 
+import com.min01.minsenchantments.enchantment.AbstractCustomEnchantment;
+
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-public abstract class AbstractEndEnchantment extends Enchantment implements IEndEnchantment
+public abstract class AbstractEndEnchantment extends AbstractCustomEnchantment implements IEndEnchantment
 {
 	public AbstractEndEnchantment(Rarity p_44676_, EnchantmentCategory p_44677_, EquipmentSlot[] p_44678_) 
 	{
--- src/main/java/com/min01/minsenchantments/enchantment/sculk/AbstractSculkEnchantment.java
+++ src/main/java/com/min01/minsenchantments/enchantment/sculk/AbstractSculkEnchantment.java
@@ -1,10 +1,11 @@
 package com.min01.minsenchantments.enchantment.sculk;
 
+import com.min01.minsenchantments.enchantment.AbstractCustomEnchantment;
+
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-public abstract class AbstractSculkEnchantment extends Enchantment implements ISculkEnchantment
+public abstract class AbstractSculkEnchantment extends AbstractCustomEnchantment implements ISculkEnchantment
 {
 	public AbstractSculkEnchantment(Rarity p_44676_, EnchantmentCategory p_44677_, EquipmentSlot[] p_44678_)
 	{
